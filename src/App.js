090ebd00d33302a700b3a2474f3bfff69a2d79f7
navneet <navneetdodani@gmail.com>
2022-01-22 01:18:12 +0530

    add routing and keys in news component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import Navbar from './Components/Navbar';
 import News from './Components/News';
+import { BrowserRouter as Router ,Routes, Route, Link } from 'react-router-dom';
 
 export default class App extends Component {
-  c = "navneet";
+ 
   render() {
-    return <div> 
+    return <div>
+      <Router>
       <Navbar />
-      <News country="in" category="science" pageSize={9} />
+      <Routes>
+        <Route exact path='/general' element={ <News key="general" pagesize={9} country="in" category="general" /> } /> 
+        <Route exact path='/business' element={ <News key="business" pagesize={9} country="in" category="business" /> } /> 
+        <Route exact path='/entertainment' element={ <News key="entertainment"  pagesize={9} country="in" category="entertainment" /> } /> 
+        <Route exact path='/health' element={ <News key="health" pagesize={9} country="in" category="health" /> } /> 
+        <Route exact path='/science' element={ <News key="science"  pagesize={9} country="in" category="science" /> } /> 
+        <Route exact path='/sports' element={ <News key="sports"  pagesize={9} country="in" category="sports" /> } /> 
+        <Route exact path='/technology' element={ <News key="technology" pagesize={9} country="in" category="technology" /> } /> 
+      </Routes>
+      </Router>
     </div>;
   }
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 export class Navbar extends Component {
   static propTypes = {};
 
@@ -15,28 +15,25 @@ export class Navbar extends Component {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">General</a>
+          <Link className="nav-link active" aria-current="page" to="/general">General</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/about">Business</a>
+          <Link className="nav-link" to="/business">Business</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/about">Entertainment</a>
+          <Link className="nav-link" to="/entertainment">Entertainment</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/about">General</a>
+          <Link className="nav-link" to="/health">Health</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/about">Health</a>
+          <Link className="nav-link" to="/science">Science</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/about">Science</a>
+          <Link className="nav-link" to="/sports">Sports</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/about">Sports</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/about">Technology</a>
+          <Link className="nav-link" to="/technology">Technology</Link>
         </li>
       </ul>
     </div>
