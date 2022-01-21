defce2035b648dcc4433ceaffa3105c18a85e95d
navneet <navneetdodani@gmail.com>
2022-01-21 23:55:47 +0530

    add spinner

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Newsitem from './Newsitem';
+import Spinner from './Spinner';
 
 export class News extends Component {
   constructor(){
@@ -15,37 +16,47 @@ export class News extends Component {
   handleNext = async () =>{
     
     if( Math.ceil(this.state.totalResults/9 > this.state.page)){
+        this.setState({loading:true})
         const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=299c6850d4e94ac88e016d957f1016e2&pageSize=9&page=${this.state.page+1}`
         const data = await fetch(url);
         const parsedData = await data.json();
-        this.setState({articles:parsedData.articles , page:this.state.page+1});
+        this.setState({
+            articles:parsedData.articles , 
+            page:this.state.page+1,
+            loading:false});
     }
     else
         console.log('no pages left');
   } 
   handlePrev = async () =>{
       if(this.state.page>1){
+        this.setState({loading:true})  
         const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=299c6850d4e94ac88e016d957f1016e2&pageSize=9&page=${this.state.page-1}`
         const data = await fetch(url);
         const parsedData = await data.json();
-        this.setState({articles:parsedData.articles , page:this.state.page-1});          
+        this.setState({articles:parsedData.articles , page:this.state.page-1,
+            loading: false});          
       }
       console.log('prev');
   } 
 
   async componentDidMount(){
+      this.setState({loading:true})
       const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=299c6850d4e94ac88e016d957f1016e2&pageSize=9&page=${this.state.page}`
       const data = await fetch(url);
       const parsedData = await data.json();
-      this.setState({articles:parsedData.articles , totalResults:parsedData.totalResults});
-      console.log(this.state.totalResults);
+      this.setState({articles:parsedData.articles , 
+                    totalResults:parsedData.totalResults,
+                    loading: false});
+
   }
   render() {
     return <div className='container my-3 '>
         <h2 className='my-3 text-center display-4 my-5'>Top Headlines </h2> 
+        {this.state.loading && <Spinner />}
         <div className="row">
-            { this.state.articles.map(({title,description,url,urlToImage})=>{
-                return <div class="col-lg-4 d-flex align-items-stretch">
+            { !this.state.loading && this.state.articles.map(({title,description,url,urlToImage})=>{
+                return <div className="col-lg-4 d-flex align-items-stretch">
                      <Newsitem title={title} desc={description} imageUrl={urlToImage} newsUrl = {url}/>
                 </div>
             })}
